0455ce82793c2f7f3410bb496f2ba12b34e1bd4d
lumin <527724143@qq.com>
2023-04-03 15:33:17 +0800

    feat:  新增特性

--- src/plugins/git.ts
+++ src/plugins/git.ts
@@ -1,3 +1,5 @@
+
+
 import { execa } from 'execa';
 import { Argv } from 'yargs';
 function gitPlugin(yargs: Argv) {
@@ -37,44 +39,6 @@ function gitPlugin(yargs: Argv) {
       }
     }
   })
-}
-
-const command = 'commit [files...]';
-const describe = 'Commit changes to Git';
-
-const builder = (yargs: Argv) => {
-  yargs.positional('files', {
-    describe: 'Files to be committed',
-    type: 'string',
-    default: '.'
-  });
-  yargs.option('push', {
-    describe: 'Push changes to Git remote',
-    type: 'boolean',
-    default: false
-  });
-  yargs.option('message', {
-    describe: 'Commit message',
-    type: 'string',
-    default: 'Update'
-  });
-  return yargs;
-}
-
-
-const handler = async (argv) => {
-  try {
-    const files = argv.files?.join(' ') || '.';
-    await execa('git', ['add', files]);
-    await execa('git', ['commit', '-m', argv.message]);
-    console.log(`Changes (${files}) have been committed to Git with message "${argv.message}".`);
-    if (argv.push) {
-      await execa('git', ['push']);
-      console.log('Changes have been pushed to Git remote.');
-    }
-  } catch (error) {
-    console.error('Failed to commit changes to Git:', error);
-  }
 }
 
 export default gitPlugin;
--- webpack.config.ts
+++ webpack.config.ts
@@ -10,15 +10,22 @@ const config: webpack.Configuration = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.cjs$/,
+        type: 'javascript/auto',
+      },
     ],
   },
   devtool: 'inline-source-map',
-  externals: ["yargs", "execa"],
+  // externals: ["yargs", "execa"],
+  externalsPresets: {
+    node: true // in order to ignore built-in modules like path, fs, etc. 
+  },
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, 'src'),
     },
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.cjs', '.mjs'],
   },
   output: {
     filename: "command.js",
