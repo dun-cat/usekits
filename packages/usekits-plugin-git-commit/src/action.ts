908eeed3fd7336599a7acc5ece09d888077ae9bf
lumin <527724143@qq.com>
2023-04-07 21:45:37 +0800

    edit

---
+++ packages/usekits-plugin-git-commit/src/action.ts
@@ -0,0 +1,79 @@
+const execaPromise = import('execa')
+import cwd from '@usekits/cli/src/utils/cwd';
+import log from '@usekits/cli/src/utils/log';
+import { copyFileSync, existsSync } from 'fs-extra';
+import { resolve } from 'path';
+import { choices } from './ui';
+let isProjectGit: boolean;
+
+async function hasProjectGit() {
+  if (isProjectGit != null) {
+    return isProjectGit;
+  }
+  try {
+    await (await execaPromise).execa('git', ['status'], { cwd: cwd.get() });
+    isProjectGit = true;
+    return isProjectGit;
+  } catch (error) {
+    isProjectGit = false;
+    return isProjectGit;
+  }
+}
+
+/**
+ * 初始化 git 项目
+ */
+async function init() {
+  try {
+    if (!existsSync(`${cwd.get()}/.gitignore`)) {
+      copyFileSync(resolve(__dirname, '../template/.gitignore'), `${cwd.get()}/.gitignore`);
+    }
+    await (await execaPromise).execa('git', ['init'], { cwd: cwd.get() });
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function commit(answers) {
+  let message = answers.msg;
+  if (answers.type) {
+    message = `${answers.type}: ${answers.msg || "code updated"}`;
+  }
+  try {
+    await (await execaPromise).execa('git', ['add', '*'], { cwd: cwd.get() });
+    const { stdout } = await (await execaPromise).execa('git', ['commit', '-m', message.replace(/"/, '\\"')], {
+      cwd: cwd.get(),
+    });
+    log.info(stdout)
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function push() {
+  try {
+    await (await execaPromise).execa('git', ['push'], { cwd: cwd.get() });
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function syncProjectToRemoteGitRepo(host, namespace, name) {
+  try {
+    commit({ type: 'feat', msg: '初始化' });
+    await (await execaPromise).execa('git', [
+      'push', '--set-upstream',
+      `${host}/${namespace}/${name}.git`, 'master',
+    ], { cwd: cwd.get() });
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default {
+  hasProjectGit,
+  commit,
+  push,
+  init,
+  syncProjectToRemoteGitRepo,
+};
---
+++ packages/usekits-plugin-git-commit/src/ui.ts
@@ -0,0 +1,75 @@
+import chalk from 'chalk';
+
+export const choices = [
+  {
+    name: '* feat     : 新特性',
+    value: 'feat',
+    defaultValue: 'feat: ',
+  },
+  {
+    name: '* fixed    : Bug 修复',
+    value: 'fixed',
+    defaultValue: 'fixed: ',
+  },
+  {
+    name: '* style    : 样式修复',
+    value: 'style',
+    defaultValue: 'style: ',
+  },
+  {
+    name: '* perf     : 性能优化',
+    value: 'perf',
+    defaultValue: 'perf: ',
+  },
+  {
+    name: '* release  : 版本发布',
+    value: 'release',
+    defaultValue: 'release: ',
+  },
+  {
+    name: '* refactor : 代码重构',
+    value: 'refactor',
+    defaultValue: 'refactor: ',
+  },
+  {
+    name: '* test     : 测试用例更新',
+    value: 'test',
+    defaultValue: 'test: ',
+  },
+  {
+    name: '* revert   : 代码回滚',
+    value: 'revert',
+    defaultValue: 'revert: ',
+  },
+  {
+    name: '* temp     : 临时提交',
+    value: 'temp',
+    defaultValue: 'temp: ',
+  },
+];
+
+export default {
+  commit: [
+    {
+      type: 'autocomplete',
+      name: 'type',
+      message: `请选择 ${chalk.yellow('commit')} 类型？`,
+      source(answersSoFar: object, input: string) {
+        return new Promise(((resolve) => {
+          const result = choices.filter(_ => _.name.indexOf(input || '') !== -1);
+          resolve(result);
+        }));
+      },
+    },
+    {
+      type: 'input',
+      name: 'msg',
+      message: `请输入 ${chalk.yellow('commit')} 信息: `,
+    },
+  ],
+  push: {
+    type: 'confirm',
+    name: 'next',
+    message: `是否 ${chalk.yellow('push')} 到远程仓库？`,
+  },
+};
